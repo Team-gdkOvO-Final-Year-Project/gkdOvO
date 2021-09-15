ba288c2f340bf80df26976101a39556ecced6707
zjy99 <jingyi.zhou.2018@scis.smu.edu.sg>
2021-09-16 01:07:10 +0800

    Update backend.py
    fixed upload button bug and preview page redirection

--- Application/backend.py
+++ Application/backend.py
@@ -20,37 +20,45 @@ def upload():
     
     #receive data from html, read csv file and convert to dataframe
     file = request.files['file']
-    csv_data = pd.read_csv(file)
-    preview_table = csv_data[0:23]    
-    
-    #drafting new preview.html
-    with open('Application/UI_pages/preview_script.txt', 'wt') as f:
-        # read_data = f.read()
-        # f.seek(0)
-        f.truncate()   #清空文件
-        # # f.write(read_data.replace(preview_table.to_html()))
-        f.writelines(preview_table.to_html())
-    content = []
-    with open('Application/UI_pages/preview_head.txt') as f:
-        lines = f.readlines()
-        for i in lines: 
-            content.append(i)
-    with open('Application/UI_pages/preview_script.txt') as f:
-        lines = f.readlines()
-        for l in lines: 
-            content.append(l)
-    with open('Application/UI_pages/preview_tail.txt') as f:
-        lines = f.readlines()
-        for l in lines: 
-            content.append(l)
-
-    with open('Application/UI_pages/preview.txt', 'a') as f:
-        f.writelines('\n'.join(content))
+    try:
+        csv_data = pd.read_csv(file)
+        preview_table = csv_data[0:23]    
         
-    os.rename('Application/UI_pages/preview.txt','Application/UI_pages/preview.html')
-    # os.replace('preview_script.html','/Application/UI_pages/preview_script.html')
-    return redirect('/preview')
-    # return('ok')
+        #drafting new preview.html
+        with open('Application/UI_pages/preview_script.txt', 'wt') as f:
+            # read_data = f.read()
+            # f.seek(0)
+            f.truncate()   #清空文件
+            # # f.write(read_data.replace(preview_table.to_html()))
+            f.writelines(preview_table.to_html())
+        content = []
+        with open('Application/UI_pages/preview_head.txt') as f:
+            lines = f.readlines()
+            for i in lines: 
+                content.append(i)
+        with open('Application/UI_pages/preview_script.txt') as f:
+            lines = f.readlines()
+            for l in lines: 
+                content.append(l)
+        with open('Application/UI_pages/preview_tail.txt') as f:
+            lines = f.readlines()
+            for l in lines: 
+                content.append(l)
+
+        with open('Application/UI_pages/preview.txt', 'a') as f:
+            f.writelines('\n'.join(content))
+            
+        os.rename('Application/UI_pages/preview.txt','Application/UI_pages/preview.html')
+        # os.replace('preview_script.html','/Application/UI_pages/preview_script.html')
+        return ('Please click Preview button')
+    except:
+        return('no data')
+    # else:
+    #     file = request.files['file']
+    #     csv_data = pd.read_csv(file)
+        
+    # print(csv_data)
+    # return(csv_data)
 
 @app.route('/preview')
 def preview():    
