630a5314d0991c6b11a9a8153391d3436f92af09
zjy99 <jingyi.zhou.2018@scis.smu.edu.sg>
2021-11-18 09:09:38 +0800

    enable display KPI in web page
    enable display KPI in web page

--- Application/backend.py
+++ Application/backend.py
@@ -5,8 +5,13 @@ from typing import Any, Dict, Optional
 from datetime import datetime
 import os
 from flask.helpers import flash
-from tkinter import messagebox
+from tkinter import Variable, messagebox
 import pandas as pd
+from pandas.core.frame import DataFrame
+from pandas.plotting import  table
+import matplotlib.pyplot as plt
+import six
+import numpy as np
 
 #function
 from pre_processing import pre_processing
@@ -17,7 +22,7 @@ from matched_selection_kpi_prediction import MatchedShopSelection_KPIPrediction
  
 app = Flask(__name__,template_folder='UI_pages')
 
-
+overall_KPI = 0
 def transform(text_file_contents):
     return text_file_contents.replace("=", ",")
 
@@ -28,6 +33,29 @@ def generate_download_headers(
     content_disp = f"attachment; filename={filename}.{extension}"
     headers = {"Content-Disposition": content_disp}
     return headers
+
+def render_mpl_table(data, col_width=3.0, row_height=0.625, font_size=14,
+                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
+                     bbox=[0, 0, 1, 1], header_columns=0,
+                     ax=None, **kwargs):
+    if ax is None:
+        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+        fig, ax = plt.subplots(figsize=size)
+        ax.axis('off')
+
+    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
+
+    mpl_table.auto_set_font_size(False)
+    mpl_table.set_fontsize(font_size)
+
+    for k, cell in  six.iteritems(mpl_table._cells):
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
+    return ax
 
 
 
@@ -113,7 +141,13 @@ def analyse():
         global selected_matched_data
 
         selected_matched_data , overall_KPI=MatchedShopSelection_KPIPrediction(matched_shops,unmatched_selected_shop,whitelist_filled,url_prefix)
-        
+        KPI = {'KPI':overall_KPI}
+        KPI = pd.DataFrame(KPI,index = [0])
+
+        kpi_png = render_mpl_table(KPI, header_columns=0, col_width=2.0)
+        kpi_png.get_figure().savefig(url_prefix+'KPI.png')
+
+
         return ('Analysis result is successfully generated, please go back to analysis page')#render_template("charts.html")
     except:
         return ("no data")
@@ -150,8 +184,7 @@ def export():
         mimetype="application/csv",
     )
 
-    
- 
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
     
